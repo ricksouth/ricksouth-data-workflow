fd105bf5a779edc3032b19176139e2aa0e267190
Rick South <rick@natamus.com>
2022-09-14 11:35:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
-sys.path.insert(0, '/membership')
-import update_membership_data
+#sys.path.insert(0, '/membership')
+#import update_membership_data
 
 def main():
     print("Starting the Python automated workflow.")
-    
-    update_membership_data.main()
+
+    #update_membership_data.main()
 
     return
 
